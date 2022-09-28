dcd2d1c7d8a5c54e681a709b064797061e82ae65
tonyfilho <oficinadosdrones@gmail.com>
2022-09-28 14:20:50 +0100

    created all the compoments

---
+++ src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-item',
+  templateUrl: './recipes-item.component.html',
+  styles: [
+  ]
+})
+export class RecipesItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
