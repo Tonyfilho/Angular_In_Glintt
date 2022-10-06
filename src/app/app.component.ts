da5b30865a7a5b9e8b72a16f8eee335d714322cd
tonyfilho <oficinadosdrones@gmail.com>
2022-10-06 08:58:13 +0100

    class nº 85/86 created basic route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularGuide2022';
+  loadedFeature: string = 'recipe';
+
+  onNavigateHeader(event: string) {
+    this.loadedFeature = event;
+  }
 }
