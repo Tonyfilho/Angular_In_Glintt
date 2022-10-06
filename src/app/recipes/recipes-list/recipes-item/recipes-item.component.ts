f974ffd89c7153568d0c70bdfb513d7eacafc3a4
tonyfilho <oficinadosdrones@gmail.com>
2022-10-06 09:42:48 +0100

    class nº 87

--- src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
+++ src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipes-item',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [ './recipes-item.Component.css']
 })
 export class RecipesItemComponent implements OnInit {
-
+  @Input("items") recipesItem!: {name: string, imagePath:string, description:string} ;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getItem(oneRecipes:{name: string, imagePath:string, description:string}) {
+  console.log("Item: " , oneRecipes);
+  }
 }
