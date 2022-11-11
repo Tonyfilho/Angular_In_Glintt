df87515b0156e09303a67908cc0d7a81824e3040
tonyfilho <oficinadosdrones@gmail.com>
2022-11-11 09:51:26 +0000

    fixing app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+ 
   {path: 'shopping', loadChildren: () => import('./shopping-list/shopping-list.module').then(module => module.ShoppingListModule)},
-  {path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(module => module.RecipesModule)}
+  {path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(module => module.RecipesModule)},
+  {path: '**', redirectTo:'', pathMatch: 'full'}
 ];
 
 @NgModule({
