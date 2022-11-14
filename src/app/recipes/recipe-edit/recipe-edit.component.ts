3ceb6bd01b7d549567800227142190f7950df4bd
tonyfilho <oficinadosdrones@gmail.com>
2022-11-14 09:47:14 +0000

    update form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,3 +1,4 @@
+import { IngredientsModel } from './../../../assets/models/ingredients.model';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipesModel } from './../../../assets/models/recipes.model';
 import { RecipesService } from './../recipes.service';
@@ -18,7 +19,7 @@ export class RecipeEditComponent implements OnInit, AfterViewInit {
   newRecipes!:  Partial<RecipesModel>;
   constructor(private fb: FormBuilder, private recipesService: RecipesService, private route: ActivatedRoute) {
    this.newOrEditRecipesForm = fb.group({
-     id: [''],
+     id: [{value: '' ,disabled: true}],
      name: [''],
      description: [''],
      imagePath: [''],
@@ -35,9 +36,10 @@ export class RecipeEditComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit(): void {
-    
+
    // this.route.params.subscribe((data:Params) => { return this.changeButtonName(+ data['id'])});
     this.idRecipe = +this.route.snapshot.params['id'];
+    this.newOrEditRecipesForm.get("id")?.setValue(this.idRecipe);
     console.log('nameButton',this.idRecipe)
     this.changeButtonName(this.idRecipe);
 
@@ -45,9 +47,23 @@ export class RecipeEditComponent implements OnInit, AfterViewInit {
 
   changeButtonName(id: number): string {
     if(id) {
+      this.updateForm();
       return   this.nameButton = 'Update Recipes' ;
     }
     return  this.nameButton = 'Save a New Recipes';
+  }
+
+  updateForm(){
+    this.recipesService.getReceipesById(this.idRecipe).subscribe((recipe: RecipesModel) => {
+      this.newOrEditRecipesForm.get("name")?.patchValue(recipe.name);
+      this.newOrEditRecipesForm.get("description")?.patchValue(recipe.description);
+      this.newOrEditRecipesForm.get("imagePath")?.patchValue(recipe.imagePath);
+      recipe.ingredients.map((incredient: IngredientsModel) => {
+        this.newOrEditRecipesForm.get("ingredients:ingredientesName")?.patchValue(incredient.name);
+        this.newOrEditRecipesForm.get("ingredients:amount")?.patchValue(incredient.amount);
+      });
+
+    });
   }
 
 }
