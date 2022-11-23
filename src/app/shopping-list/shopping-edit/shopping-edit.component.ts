971862dccb45fe4d26596977e04aa5cf3d219bd7
tonyfilho <oficinadosdrones@gmail.com>
2022-11-23 10:01:37 +0000

    valitators in shopping-edit.component

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
+import { CustomValidation } from './../../_share/custom-validators/Custom-Validation';
 import { ShoppingListService } from './../shopping-list.service';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IngredientsModel } from 'src/assets/models/ingredients.model';
 
 @Component({
@@ -11,25 +12,30 @@ import { IngredientsModel } from 'src/assets/models/ingredients.model';
 })
 export class ShoppingEditComponent implements OnInit {
   @Output("loadForm") loadForm = new EventEmitter<IngredientsModel>()
+  localPattern= "^[1-9]+[0-9]*$";
   myFormBuilder: FormGroup;
   constructor(private fb: FormBuilder, private shopService: ShoppingListService) {
     this.myFormBuilder = fb.group({
-      ingred_name:[''],
-     amount:['']
+      ingred_name:['',[Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+     amount:['', [Validators.required, CustomValidation.justanumber, Validators.pattern(this.localPattern)]]
     })
    }
 
   ngOnInit(): void {
   }
 
-  submitForms(localFormBuilder: FormGroup) {
+  submitForms() {
     /**Foi Adcionado um Service sem precisar emitir dados localmente
     this.loadForm.emit({name: localFormBuilder.get("name")?.value, amount: localFormBuilder.get("amount")?.value });
      */
   // this.shopService.addIncredient.emit({name: localFormBuilder.get("name")?.value, amount: localFormBuilder.get("amount")?.value });
-  this.shopService.plusIngredient({ingred_name: localFormBuilder.get("ingred_name")?.value, amount: localFormBuilder.get("amount")?.value });
+  this.shopService.plusIngredient({ingred_name: this.myFormBuilder.get("ingred_name")?.value, amount: this.myFormBuilder.get("amount")?.value });
 
     this.myFormBuilder.reset();
   }
 
+  deleteIngredient() {
+
+
+  }
 }
